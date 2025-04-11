5e018647f06319c88fb473d91dfa21a084973695
bhartl <hartl.bene@gmail.com>
2025-04-11 19:53:13 +0200

    mujoco envs

--- examples/gym/experiments/__init__.py
+++ examples/gym/experiments/__init__.py
@@ -8,9 +8,24 @@ from .cartpole import Cartpole
 from .lunar_lander import LunarLander
 from .lunar_lander import LunarLanderContinuous
 
+# mujoco
+from .mujoco_swimmer import MujocoSwimmer
+from .mujoco_hopper import MujocoHopper
+from .mujoco_half_cheetah import MujocoHalfCheetah
+from .mujoco_ant import MujocoAnt
+from .mujoco_humanoid import MujocoHumanoid
+
 __all__ = [
+    # classic control
     "Acrobot",
     "Cartpole",
+    # box2d
     "LunarLander",
     "LunarLanderContinuous",
+    # mujoco
+    "MujocoSwimmer",
+    "MujocoHopper",
+    "MujocoHalfCheetah",
+    "MujocoAnt",
+    "MujocoHumanoid",
 ]

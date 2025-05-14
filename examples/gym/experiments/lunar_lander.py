0dfaafff7817267eca97b6cd274b1d8e1c98f318
bhartl <hartl.bene@gmail.com>
2025-05-14 09:27:38 +0200

    model eval/train mode fix

--- examples/gym/experiments/lunar_lander.py
+++ examples/gym/experiments/lunar_lander.py
@@ -21,7 +21,3 @@ class LunarLanderContinuous(ABCExperiment):
     GYM_ID = "LunarLanderContinuous-v2"
     def __init__(self, **kwargs):
         super().__init__(gym_id=self.GYM_ID, **kwargs)
-
-    @property
-    def num_conditions(self):
-        return 0

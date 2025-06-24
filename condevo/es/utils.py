e4686d830d02c83206aef5db9f4c4037fa19691b
bhartl <hartl.bene@gmail.com>
2025-06-24 14:20:26 +0200

    modular selection workaround

--- condevo/es/utils.py
+++ condevo/es/utils.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from functools import partial
+from .selection import roulette_wheel, boltzmann_selection  # legacy import for compatibility
 
 
 def tensor_to_numpy(t: torch.Tensor):
@@ -139,138 +140,6 @@ class ScheduledSelectionPressure:
     # override left-side multiplication with numpy array
     def __lmul__(self, other):
         return self.get_value() * other
-
-
-@torch.no_grad()
-def roulette_wheel(f, s=3., eps=1e-12, assume_sorted=False, normalize=False, consider_nans=False, threshold=0):
-    """ Roulette wheel fitness transformation.
-
-    We transform the fitness values f to probabilities p by applying the roulette wheel fitness transformation.
-    The roulette wheel fitness transformation is a monotonic transformation that maps the fitness values to
-    probabilities. The selection pressure s controls the degree of selection. The higher the selection pressure,
-    the more the probabilities are concentrated on the best solutions (s can be positive or negative).
-
-    :param f: torch.Tensor of shape (popsize,), fitness values of the sampled solutions
-    :param s: float, selection pressure
-    :param eps: float, epsilon to avoid division by zero
-    :param assume_sorted: bool, whether to disable sorting of the fitness values and assume that they are already sorted
-    :param normalize: bool, whether to normalize the probabilities to sum to 1 (default False, i.e., the sum over
-                      the returned scaled probabilities is equal to the sum over the fitness absolute values)
-    :param consider_nans: bool, whether to consider NaN fitness values in the selection process
-    :return: torch.Tensor of shape (popsize,), indices of the selected solutions
-    """
-    if not isinstance(f, (torch.Tensor, np.ndarray)):
-        f = torch.tensor(f)
-
-    f_nan = torch.isnan(f) if isinstance(f, torch.Tensor) else np.isnan(f)
-    f_inf = torch.isinf(f) if isinstance(f, torch.Tensor) else np.isinf(f)
-    f_valid = ~(f_nan | f_inf)
-
-    f_finite = torch.zeros_like(f) if isinstance(f, torch.Tensor) else np.zeros_like(f)
-    if not consider_nans:
-        f = f[f_valid]
-
-    if threshold > 0:
-        f_threshold = f.min() + (f.max() - f.min()) * threshold
-        f[f < f_threshold] = f_threshold  # apply threshold to fitness values
-
-    exp = torch.exp if isinstance(f, torch.Tensor) else np.exp
-    device = f.device if isinstance(f, torch.Tensor) else 'cpu'
-    indices = torch.arange(len(f), device=device) if isinstance(f, torch.Tensor) else np.arange(len(f))
-
-    if not assume_sorted:
-        # sort fitness in ascending order
-        if isinstance(f, torch.Tensor):
-            asc = torch.argsort(f.flatten(), descending=False, dim=0)
-            where = torch.where
-        else:  # numpy
-            asc = f.flatten().argsort()
-            where = np.where
-
-        indices = where(asc[None, :] == indices[:, None])[1]  # original order
-        f = f[asc]
-
-    unique_f = (torch.unique(f) if isinstance(f, torch.Tensor) else np.unique(f))
-    if isinstance(f, torch.Tensor):
-        total_weight = torch.abs(f).sum()
-        fs = torch.zeros_like(f)
-        unique_mapping = [(torch.where(uf == f)[0]).flatten() for uf in unique_f]
-    else:
-        total_weight = np.abs(f).sum()
-        fs = np.zeros_like(f)
-        unique_mapping = [(np.where(uf == f)[0]).flatten() for uf in unique_f]
-
-    f_scaled = (unique_f - f.min()) / (f.max() - f.min() + eps)  # normalize fitness values to [0, 1], and sort
-    f_scaled = exp(s*f_scaled)  # apply selection pressure, s can be positive or negative
-    f_scaled = f_scaled.cumsum(dim=0) if isinstance(f_scaled, torch.Tensor) else np.cumsum(f_scaled) # compute cumulative sum
-    f_scaled += eps
-
-    if len(unique_mapping) != len(fs):
-        # remap to original lengths (allow duplicates)
-        for i, um in enumerate(unique_mapping):
-            fs[um] = f_scaled[i]
-    else:
-        fs = f_scaled
-
-    fs /= fs.sum()
-    if not normalize:
-        fs *= total_weight
-
-    fs = fs[indices]
-    if consider_nans:
-        f_finite = fs
-    else:
-        f_finite[f_valid] = fs
-    return f_finite
-
-
-@torch.no_grad()
-def boltzmann_selection(f, s=3., normalize=False, consider_nans=False, threshold=0, eps=0.):
-    """ Roulette wheel fitness transformation.
-
-    We transform the fitness values f to probabilities p by applying the roulette wheel fitness transformation.
-    The roulette wheel fitness transformation is a monotonic transformation that maps the fitness values to
-    probabilities. The selection pressure s controls the degree of selection. The higher the selection pressure,
-    the more the probabilities are concentrated on the best solutions (s can be positive or negative).
-
-    :param f: torch.Tensor of shape (popsize,), fitness values of the sampled solutions
-    :param s: float, selection pressure, corresponds to inverse temperature.
-    :param threshold: float, fitness cutoff ratio (highest - lowest) which are considered as "finite" weights.
-    :param eps: float, epsilon weight for samples below threshold.
-    :param normalize: bool, whether to normalize the probabilities to sum to 1 (default False, i.e., the sum over
-                      the returned scaled probabilities is equal to the sum over the fitness absolute values)
-    :param consider_nans: bool, whether to consider NaN fitness values in the selection process
-    :return: torch.Tensor of shape (popsize,), indices of the selected solutions
-    """
-    if not isinstance(f, (torch.Tensor, np.ndarray)):
-        f = torch.tensor(f)
-
-    f_nan = torch.isnan(f) if isinstance(f, torch.Tensor) else np.isnan(f)
-    f_inf = torch.isinf(f) if isinstance(f, torch.Tensor) else np.isinf(f)
-    f_valid = ~(f_nan | f_inf)
-
-    f_finite = torch.zeros_like(f) if isinstance(f, torch.Tensor) else np.zeros_like(f)
-    if not consider_nans:
-        f = f[f_valid]
-
-    exp = torch.exp if isinstance(f, torch.Tensor) else np.exp
-    abs = torch.abs if isinstance(f, torch.Tensor) else np.abs
-    f_max = f.max() if isinstance(f, torch.Tensor) else np.max(f)
-    fs = exp(-s * abs(f - f_max))  # apply Boltzmann selection pressure, s can be positive or negative
-
-    if threshold > 0:
-        f_threshold = f.min() + (f.max() - f.min()) * threshold
-        fs[f < f_threshold] = eps  # apply threshold to fitness values
-
-    if normalize:
-        fs /= fs.sum()
-
-    if consider_nans:
-        f_finite = fs
-    else:
-        f_finite[f_valid] = fs
-
-    return f_finite
 
 
 def parameter_crowding(parameters, weight=1., sharpness=1., similarity_metric="euclidean"):
